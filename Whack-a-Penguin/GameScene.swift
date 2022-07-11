bfdcc7a2a7e870e0e6785fca2c12f67d15052cbd
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-10 19:45:20 -0500

    first commit

---
+++ Whack-a-Penguin/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Whack-a-Penguin
+//
+//  Created by Camilo Hernández Guerrero on 10/07/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
